078db2072748464acebcd1f6bc9f5b255ca927ae
Omar El Founti Khsim <omar.elfounti@salesianos.lasnaves>
2025-01-15 19:30:01 +0100

    Paso 3

--- servidor.js
+++ servidor.js
@@ -1,7 +1,47 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
+
+
+    res.setHeader("Content-Type", "text/html");
+    //res.write("hola clase");
+    //res.write("<p> ejemplo de node</p>");
+    //res.write("<p> ejemplo de node</p>");
+    //res.end();
+
+    let ruta = "./visualizacion/";
+    switch (req.url) {
+        case "/":
+            ruta += "index.html";  
+            break;
+        case "/about":
+            ruta += "about.html";  
+            break;
+        default:
+            ruta += "404.html";
+            break;
+    }
+
+
+    
+    
+    
+    
+    
+    
+    fs.readFile(ruta, (err, data) => {
+        if(err){
+            console.log(err);
+            res.end();
+            
+        } else{
+            res.write(data);
+            res.end();
+        }
+    })
+
     
 });
 
